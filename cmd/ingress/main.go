9d57e530db227bf37934ecb79102d49bf0e4b261
Alex J <alex@localhost.localdomain>
2019-08-22 22:40:15 -0400

    Testing stuff D:

--- cmd/ingress/main.go
+++ cmd/ingress/main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"fmt"
+	"github.com/open-bastion/open-bastion/internal/config"
+	"os"
 )
 
 func main() {
-	fmt.Println("ingress : Hello world!")
+	args := os.Args
+
+	config := new(config.Config)
+	config.ParseConfig(args[1])
+
+	fmt.Println(config.PermitPasswordLogin)
 }
